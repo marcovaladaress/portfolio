f310f581df078ce60a908efe8a83bd932412bdfa
marco valadares <marcovf99@gmail.com>
2026-02-05 21:51:46 -0300

    feat: project setup configuration and page header creation

---
+++ app/_components/header.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { TextAlignJustify } from "lucide-react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="flex items-center justify-between p-5">
+          <div className="flex items-center gap-2">
+            <div className="bg-primary flex h-7 w-7 items-center justify-center rounded">
+              <p className="text-center text-sm font-bold">D</p>
+            </div>
+            <h1 className="text-[14px] font-medium">Dev.Portfólio</h1>
+          </div>
+          <Button variant={"ghost"}>
+            <TextAlignJustify />
+          </Button>
+        </div>
+      </header>
+      <Separator />
+    </>
+  );
+};
+
+export default Header;
