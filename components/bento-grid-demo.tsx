2f8bd2fb35fb8c91c5e56f1145d67620cc3de663
marco valadares <marcovf99@gmail.com>
2026-05-12 18:34:18 -0300

    fix: replace webAi.png with Wb.png in BentoGrid demo component

--- components/bento-grid-demo.tsx
+++ components/bento-grid-demo.tsx
@@ -40,7 +40,7 @@ const items = [
       "Projeto de estudo focado em uma interface moderna, minimalista e responsiva.",
     header: (
       <Image
-        src="/webAi.png"
+        src="/Wb.png"
         alt="WebAI"
         width={500}
         height={300}
